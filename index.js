053dfe0316d45fbee8c04b2013f4d4835168d35d
rubencosta <rmc@nubolab.org>
2016-01-25 16:57:02 +0000

    auth: implement jwt validation + authenticate endpoint + use jwt user as graphql rootValue

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import jwt from 'jsonwebtoken'
+import expressJwt from 'express-jwt'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import GraphQLHTTP from 'express-graphql'
 import schema from './data/schema'
+import { getUserByUsername } from './data/models/user'
+
+const secret = 'secret'
 
 const app = express()
 
@@ -17,4 +23,35 @@ db.once('open', () => {
 
 app.options('*', cors())
 app.use(cors())
-app.use('/graphql', GraphQLHTTP({schema, graphiql: true}))
+
+app.use(
+  '/graphql',
+  expressJwt({
+    secret,
+    credentialsRequired: false,
+  }),
+  GraphQLHTTP((req) => ({
+    schema,
+    graphiql: true,
+    rootValue: {user: req.user}
+  }))
+)
+
+app.post('/authenticate', bodyParser(), (req, res) => {
+  getUserByUsername(req.body.username)
+    .then((user) => {
+      if (user.isValidPassword(req.body.password)) {
+        return user
+      }
+      throw new Error('invalid password')
+    })
+    .then((user) => {
+      console.log(user.toObject())
+      const token = jwt.sign(user.toObject(), secret, {expiresInMinutes: 5})
+      res.json({token})
+    })
+    .catch((err) => {
+      console.error(err)
+      res.send(401, 'wrong username or password')
+    })
+})
